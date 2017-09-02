3bbae9f82e815c0704ea471e922d397762045d22
Square_wave <willmartens91@gmail.com>
2017-09-01 20:02:32 -0400

    initial excel parser complete, missing error checking, passes info to tag generator. Tag generator forthcoming

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # minitagwriter
 A quick and dirty tag making script
+
+requires the following python libraries: Pillow/xlrd
--- taggen.py
+++ taggen.py
@@ -13,7 +13,7 @@
 # small tag offsets in px: (93,258)        #
 # small tag text offset: add (10,200) to xy#
 # small tag char count: 15 uppr/17  lowr   #
-# small tag max lines: 4
+# small tag max lines: 4                   #
 # # of small tags per col/row: (5,8)       #
 # big tag info to be determined            #
 ############################################
@@ -26,9 +26,26 @@ small_canvas = Image.new("RGB",(2430,3008),(150,150,255))
 draw = ImageDraw.Draw(small_canvas)
 font = ImageFont.truetype("MyriadPro-Cond.ttf",35)
 
-def add_tag_to_canvas():
+############################################
+# add_tag_to_canvas                        #
+# adds tag of the specified type with info #
+# provided to the correct global canvas    #
+#                                          #
+# prices: tuple, 0 is normal price         #
+#                1 is sale price           #
+# text: tuple, 0 is name of item           #
+#              1 is sku of item            #
+#              2 is description of item    #
+# quantities: tuple, 0 is # of sm.reg tags #
+#                   1 is # of sm.sale tags #
+#                   2 is # of sm.clr  tags #
+#                   3 is # of lg.reg tags  #
+#                   4 is # of lg.sale tags #
+#                   5 is # of lg.clr tags  #
+#                   6 is # of parts tags   #
+############################################
+def add_tag_to_canvas(prices,text,quantities):
 
-    tag = Image.open("images/small/sclear.png")
     print "lolno"
 
 if len(sys.argv) == 2:
@@ -46,6 +63,9 @@ if len(sys.argv) == 2:
         if sheet.cell_value(row,0) != '':
             #check for valid data across the row
             #pull the info and pass to the tag maker routine
-            print "yo"
+            nprices = (sheet.cell_value(row,3),sheet.cell_value(row,4))
+            ntext = (sheet.cell_value(row,1),sheet.cell_value(row,0),sheet.cell_value(row,2))
+            nquantities = (sheet.cell_value(row,5),sheet.cell_value(row,6),sheet.cell_value(row,7),sheet.cell_value(row,8),sheet.cell_value(row,9),sheet.cell_value(row,10),sheet.cell_value(row,11))
+            add_tag_to_canvas(nprices,ntext,nquantities)
 else:
     print "incorrect number of arguments"
