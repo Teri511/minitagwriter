b3077d24415f4e8b9af47adaeb7aef866a7782c5
Square_wave <willmartens91@gmail.com>
2017-09-10 15:39:39 -0400

    minor rewrite: tag info now pulled from a database file instead of being manually entered by the user

--- taggen.py
+++ taggen.py
@@ -36,12 +36,12 @@ big_canvas = Image.new("RGB",(2430,3108),(255,150,150))
 draw = ImageDraw.Draw(small_canvas)
 b_draw = ImageDraw.Draw(big_canvas)
 small_font = ImageFont.truetype("MyriadPro-Cond.ttf",35)
-sku_font = ImageFont.truetype("MyriadPro-Cond.ttf",20)
+sku_font = ImageFont.truetype("MyriadPro-Cond.ttf",35)
 dollar_font = ImageFont.truetype("MyriadPro-Cond.ttf",85)
 cent_font = ImageFont.truetype("MyriadPro-Cond.ttf",40)
 b_dollar_font = ImageFont.truetype("MyriadPro-Cond.ttf",220)
 b_cent_font = ImageFont.truetype("MyriadPro-Cond.ttf",110)
-extra_font = ImageFont.truetype("MyriadPro-Cond.ttf",65)
+extra_font = ImageFont.truetype("MyriadPro-Cond.ttf",55)
 b_extra_font = ImageFont.truetype("MyriadPro-Cond.ttf",190)
 small_tag_count = 0
 small_curr_row = 0
@@ -108,7 +108,7 @@ def add_tag_to_canvas(prices,text,quantities):
             cents = str(prices[0])[len(str(prices[0]))-2] + str(prices[0])[len(str(prices[0]))-1]
             #hacky check to get rid of the "no cent" bug
             if cents[0] == ".":
-                cents="00"
+                cents=cents[1] + "0"
             px,py = draw.textsize(dollar,font=dollar_font)
             #paste in the tag
             small_canvas.paste(tag, (((small_tag_count%5)*449)+93,(small_curr_row*335)+258))
@@ -117,11 +117,11 @@ def add_tag_to_canvas(prices,text,quantities):
             for line in range(0,len(name)):
                 draw.text((((small_tag_count%5)*449)+103,(small_curr_row*335)+458+(30*line)),name[line],font=small_font,fill=(0,0,0,255))
             #now the dollar
-            draw.text((((small_tag_count%5)*449)+458-px,(small_curr_row*335)+478),dollar,font=dollar_font,fill=(0,0,0,255))
+            draw.text((((small_tag_count%5)*449)+458-px,(small_curr_row*335)+463),dollar,font=dollar_font,fill=(0,0,0,255))
             #now the cents
-            draw.text((((small_tag_count%5)*449)+458,(small_curr_row*335)+478),cents,font=cent_font,fill=(0,0,0,255))
+            draw.text((((small_tag_count%5)*449)+458,(small_curr_row*335)+463),cents,font=cent_font,fill=(0,0,0,255))
             #next, the sku
-            draw.text((((small_tag_count%5)*449)+418,(small_curr_row*335)+568),str(text[1]),font=sku_font,fill=(0,0,0,255))
+            draw.text((((small_tag_count%5)*449)+398,(small_curr_row*335)+548),str(text[1])[0:3]+"-"+str(text[1])[2:6],font=sku_font,fill=(0,0,0,255))
 
             #adjust row position
             if small_tag_count%5 == 4:
@@ -149,7 +149,7 @@ def add_tag_to_canvas(prices,text,quantities):
             cents = str(prices[1])[len(str(prices[1]))-2] + str(prices[1])[len(str(prices[1]))-1]
             #hacky check to get rid of the "no cent" bug
             if cents[0] == ".":
-                cents="00"
+                cents=cents[1] + "0"
             px,py = draw.textsize(dollar,font=dollar_font)
             #paste in the tag
             small_canvas.paste(tag, (((small_tag_count%5)*449)+93,(small_curr_row*335)+258))
@@ -158,11 +158,11 @@ def add_tag_to_canvas(prices,text,quantities):
             for line in range(0,len(name)):
                 draw.text((((small_tag_count%5)*449)+103,(small_curr_row*335)+458+(30*line)),name[line],font=small_font,fill=(0,0,0,255))
             #now the dollar
-            draw.text((((small_tag_count%5)*449)+458-px,(small_curr_row*335)+498),dollar,font=dollar_font,fill=(255,255,255,255))
+            draw.text((((small_tag_count%5)*449)+458-px,(small_curr_row*335)+483),dollar,font=dollar_font,fill=(255,255,255,255))
             #now the cents
-            draw.text((((small_tag_count%5)*449)+458,(small_curr_row*335)+498),cents,font=cent_font,fill=(255,255,255,255))
+            draw.text((((small_tag_count%5)*449)+458,(small_curr_row*335)+483),cents,font=cent_font,fill=(255,255,255,255))
             #next, the sku
-            draw.text((((small_tag_count%5)*449)+418,(small_curr_row*335)+568),str(text[1]),font=sku_font,fill=(255,255,255,255))
+            draw.text((((small_tag_count%5)*449)+398,(small_curr_row*335)+548),str(text[1])[0:3]+"-"+str(text[1])[2:6],font=sku_font,fill=(255,255,255,255))
             #since its a sale tag, add the word "sale"
             draw.text((((small_tag_count%5)*449)+410,(small_curr_row*335)+438),"Sale",font=extra_font,fill=(255,255,255,255))
 
@@ -207,7 +207,7 @@ def add_tag_to_canvas(prices,text,quantities):
             cents = str(prices[0])[len(str(prices[0]))-2] + str(prices[0])[len(str(prices[0]))-1]
             #hacky check to get rid of the "no cent" bug
             if cents[0] == ".":
-                cents="00"
+                cents=cents[1] + "0"
             px,py = b_draw.textsize(dollar,font=b_dollar_font)
             #paste in the tag
             big_canvas.paste(tag, (((big_tag_count%2)*1051)+160,(big_curr_row*600)+107))
@@ -220,7 +220,7 @@ def add_tag_to_canvas(prices,text,quantities):
             #now the cents
             b_draw.text((((big_tag_count%2)*1051)+1075,(big_curr_row*600)+300),cents,font=b_cent_font,fill=(0,0,0,255))
             #next, the sku
-            b_draw.text((((big_tag_count%2)*1051)+925,(big_curr_row*600)+600),str(text[1]),font=cent_font,fill=(0,0,0,255))
+            b_draw.text((((big_tag_count%2)*1051)+925,(big_curr_row*600)+600),str(text[1])[0:3]+"-"+str(text[1])[2:6],font=cent_font,fill=(0,0,0,255))
 
             #adjust row position
             if big_tag_count%2 == 1:
@@ -250,7 +250,7 @@ def add_tag_to_canvas(prices,text,quantities):
             cents = str(prices[1])[len(str(prices[1]))-2] + str(prices[1])[len(str(prices[1]))-1]
             #hacky check to get rid of the "no cent" bug
             if cents[0] == ".":
-                cents="00"
+                cents=cents[1] + "0"
             px,py = b_draw.textsize(dollar,font=b_dollar_font)
             #paste in the tag
             big_canvas.paste(tag, (((big_tag_count%2)*1051)+160,(big_curr_row*600)+107))
@@ -263,7 +263,7 @@ def add_tag_to_canvas(prices,text,quantities):
             #now the cents
             b_draw.text((((big_tag_count%2)*1051)+1075,(big_curr_row*600)+325),cents,font=b_cent_font,fill=(255,255,255,255))
             #next, the sku
-            b_draw.text((((big_tag_count%2)*1051)+925,(big_curr_row*600)+600),str(text[1]),font=cent_font,fill=(255,255,255,255))
+            b_draw.text((((big_tag_count%2)*1051)+925,(big_curr_row*600)+600),str(text[1])[0:3]+"-"+str(text[1])[2:6],font=cent_font,fill=(255,255,255,255))
             #since its a sale tag, add the word "sale"
             b_draw.text((((big_tag_count%2)*1051)+885,(big_curr_row*600)+150),"Sale",font=b_extra_font,fill=(255,255,255,255))
 
@@ -293,23 +293,34 @@ if len(sys.argv) == 2:
     #you can ask for data all the way up to 10,10 and just get nulls
     book = xlrd.open_workbook(filename=sys.argv[1])
     sheet = book.sheet_by_index(0)
+    db_book = xlrd.open_workbook("item catalog.xlsx")
+    db_sheet = db_book.sheet_by_index(0)
+
+    catalog = {}
+
+    #build a dictionary of SKUs to their data
+    for sku in range(1,db_sheet.nrows):
+        #more error checking here
+        #print "adding sku: " + str(db_sheet.cell_value(sku,0))
+        catalog[db_sheet.cell_value(sku,0)] = [str(db_sheet.cell_value(sku,1)),str(db_sheet.cell_value(sku,2)),db_sheet.cell_value(sku,3)]
 
     #loop across each row
     for row in range(1,sheet.nrows):
         #check to make sure our users didn't skip any rows
         if sheet.cell_value(row,0) != '':
-            #check for valid data across the row
-            #pull the info and pass to the tag maker routine
-            #and mark if we need to tell the user to grab sheets of big/small/parts tag paper
-            #yes I know this is bad, I'll fix it later
-            if int(sheet.cell_value(row,5)) > 0 or int(sheet.cell_value(row,6)) > 0 or int(sheet.cell_value(row,7)) > 0:
+            #error checking goes here
+            #find the sku in the catalog and pull its info
+            #info goes: name,description,normal price
+            info = catalog[sheet.cell_value(row,0)]
+            #add the info
+            if int(sheet.cell_value(row,2)) > 0 or int(sheet.cell_value(row,3)) > 0 or int(sheet.cell_value(row,4)) > 0:
                 need_small = 1;
-            if int(sheet.cell_value(row,8)) > 0 or int(sheet.cell_value(row,9)) > 0 or int(sheet.cell_value(row,10)) > 0:
+            if int(sheet.cell_value(row,5)) > 0 or int(sheet.cell_value(row,6)) > 0 or int(sheet.cell_value(row,7)) > 0:
                 need_big = 1;
-            nprices = (sheet.cell_value(row,3),sheet.cell_value(row,4))
-            ntext = (sheet.cell_value(row,1),sheet.cell_value(row,0),sheet.cell_value(row,2))
-            nquantities = (int(sheet.cell_value(row,5)),int(sheet.cell_value(row,6)),int(sheet.cell_value(row,7)),int(sheet.cell_value(row,8)),int(sheet.cell_value(row,9)),int(sheet.cell_value(row,10)),int(sheet.cell_value(row,11)))
-            print "genning row " + str(row)
+            nprices = (info[2],sheet.cell_value(row,1))
+            ntext = (info[0],int(sheet.cell_value(row,0)),info[1])
+            nquantities = (int(sheet.cell_value(row,2)),int(sheet.cell_value(row,3)),int(sheet.cell_value(row,4)),int(sheet.cell_value(row,5)),int(sheet.cell_value(row,6)),int(sheet.cell_value(row,7)),int(sheet.cell_value(row,8)))
+            print "genning tags for sku: " + str(int(sheet.cell_value(row,0)))
             add_tag_to_canvas(nprices,ntext,nquantities)
     print "You need to grab " + str(small_page_count+need_small) + " small tag sheets and " + str(big_page_count+need_big) + " large tag sheets"
     raw_input("Press Enter to continue...")
